5dc42298d617b2e1f5de8a9e46ea5eced46ced96
Roshan Katuwal <katuwalroshan790@gmail.com>
2025-08-06 18:01:17 -0500

    server

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,28 @@
 
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (err) {
+//     console.error(`Error: ${err.message}`);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error: ${err.message}`);
@@ -15,3 +31,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
