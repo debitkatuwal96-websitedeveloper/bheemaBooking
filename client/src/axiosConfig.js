ccb5c2ff80a3cb0d52557060ac2a02d7032a7ca5
Roshan Katuwal <katuwalroshan790@gmail.com>
2025-08-07 09:27:57 -0500

    frontend api change

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/v1';
+// axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/v1';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'https://bookingserver-vdea.onrender.com';
 axios.defaults.withCredentials = true;
 
 export default axios;
--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1',
+  // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1',
+
+  baseURL: import.meta.env.VITE_API_URL || 'https://bookingserver-vdea.onrender.com',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
